d4d4ddcadce1de7835054268ef1862f132c1e330
gaurav-agarwal-cardinalts <gaurav.agarwal@triconit.com>
2019-02-14 16:33:50 +0530

    Stage 3

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,22 +2,32 @@ import path from 'path';
 import webpack from 'webpack';
 
 export default {
-    devtools: 'eval-source-map',
-    entry: path.join(__dirname, '/client/index.js'),
+   // devtools: 'eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        path.join(__dirname, '/client/index.js'),
+    ],
     output: {
         path: '/',
+        publicPath: '/',
         filename: 'bundle.js'
     },
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         rules: [
             {
                 test: /\.js$/,
                 include: path.join(__dirname, 'client'),
-                loader: 'babel-loader'
+                loaders: [ 'react-hot-loader/webpack', 'babel-loader' ]
             }
         ]
     },
     resolve: {
         extensions: [ '*', '.js' ]
-    }
+    },
+    mode: 'development'
 }
